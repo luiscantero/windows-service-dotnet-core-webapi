c0317c1a2f63b6e2672749aca3488deb5f7c3cad
Luis Cantero <luiscantero@hotmail.com>
2018-03-22 21:11:22 +0100

    Moved WebSockets echo sample to own class

--- FileWinSvcWebApi/Startup.cs
+++ FileWinSvcWebApi/Startup.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.WebSockets;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace FileWinSvcWebApi
 {
@@ -26,6 +18,7 @@ namespace FileWinSvcWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc();
         }
 
@@ -49,45 +42,13 @@ namespace FileWinSvcWebApi
 
         private void UseWebSockets(IApplicationBuilder app)
         {
-            var webSocketOptions = new WebSocketOptions()
+            app.UseWebSockets(new WebSocketOptions()
             {
                 KeepAliveInterval = TimeSpan.FromSeconds(120),
                 ReceiveBufferSize = 4 * 1024
-            };
-            app.UseWebSockets(webSocketOptions);
-
-            app.Use(async (context, next) =>
-            {
-                if (context.Request.Path == "/wsecho")
-                {
-                    if (context.WebSockets.IsWebSocketRequest)
-                    {
-                        Console.WriteLine("Echo server at: " + context.Request.PathBase);
-                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
-                        await Echo(context, webSocket);
-                    }
-                    else
-                    {
-                        context.Response.StatusCode = 400;
-                    }
-                }
-                else
-                {
-                    await next();
-                }
             });
-        }
 
-        private async Task Echo(HttpContext context, WebSocket webSocket)
-        {
-            byte[] buffer = new byte[1024 * 4];
-            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-            while (!result.CloseStatus.HasValue)
-            {
-                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
-                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-            }
-            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+            app.UseMiddleware<WebSocketsMiddleware>();
         }
     }
 }
---
+++ FileWinSvcWebApi/WebSocketsMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace FileWinSvcWebApi
+{
+    public class WebSocketsMiddleware
+    {
+        RequestDelegate _next;
+
+        public WebSocketsMiddleware(RequestDelegate next) => _next = next;
+
+        public async Task Invoke(HttpContext context, IHostingEnvironment env)
+        {
+            if (context.Request.Path == "/wsecho")
+            {
+                if (context.WebSockets.IsWebSocketRequest)
+                {
+                    Console.WriteLine("Echo server at: " + context.Request.PathBase);
+                    WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                    await Echo(context, webSocket);
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                }
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            byte[] buffer = new byte[1024 * 4];
+            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+    }
+}
